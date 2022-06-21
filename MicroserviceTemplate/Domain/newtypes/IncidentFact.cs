1e88664d47cfc1a75e635c3e388165024c9692cf
Jose Amaro <joseamaro@boldint.com>
2022-06-21 17:30:03 +0100

    Committer: Jose Amaro <joseamaro@boldint.com>  On branch archtests  Changes to be committed: 	renamed:    .gitignore.txt -> .gitignore 	deleted:    MicroserviceTemplate/API/Controllers/WeatherForecastController.cs 	deleted:    MicroserviceTemplate/Domain/WeatherForecast.cs 	deleted:    MicroserviceTemplate/Domain/newtypes/Incident.cs 	deleted:    MicroserviceTemplate/Domain/newtypes/IncidentFact.cs 	modified:   UnitTest/Architecture/ArchitectureTest.cs

--- MicroserviceTemplate/Domain/newtypes/IncidentFact.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MicroserviceTemplate.Domain.newtypes
-{
-    public class IncidentFact
-    {
-        public DateTime Date { get; set; }
-        public int NumberOfPeopleInvolved { get; set; }
-
-    }
-}
--- UnitTest/Architecture/ArchitectureTest.cs
+++ UnitTest/Architecture/ArchitectureTest.cs
@@ -1,110 +1,104 @@
-﻿
-using ArchUnitNET.Domain;
+﻿using ArchUnitNET.Domain;
 using ArchUnitNET.Loader;
 using ArchUnitNET.Fluent;
 using FluentAssertions;
-
-//add a using directive to ArchUnitNET.Fluent.ArchRuleDefinition to easily define ArchRules
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
-using MicroserviceTemplate.Application;
 using MicroserviceTemplate.Domain;
 
 namespace UnitTest
 {
     public class ArchitectureTest
     {
-        // TIP: load your architecture once at the start to maximize performance of your tests
+        private static readonly string DomainLayerRule = "Domain Layer cannot depend on outer layers.";
+        private static readonly string DomainLayerNamespace = ".Domain*";
+        private static readonly string DomainLayerName = "Domain Layer";
+
+        private static readonly string ApplicationLayerNamespace = ".Application*";
+        private static readonly string ApplicationLayerName = "Application Layer";
+
+        private static readonly string ApiLayerNamespace = ".Application*";
+        private static readonly string ApiLayerName = "API Layer";
+
+        private static readonly string InfrastructureLayerNamespace = ".Infrastructure*";
+        private static readonly string InfrastructureLayerName = "Infrastructure Layer";
+
+        private static readonly string ServiceClassesName = "Service Classes";
+        private static readonly string ServiceName = "Service";
+        private static readonly string RepositoryInterfacesName = "Repository Interfaces";
+        private static readonly string RepositoryName = "Repository";
+
+        private static readonly System.Reflection.Assembly Assembly = typeof(Incident).Assembly;        // replace Incident with generic microservice class
+
+        // load architecture once at the start to maximize performance of your tests
         private static readonly Architecture Architecture =
-            new ArchLoader().LoadAssemblies(typeof(Incident).Assembly)
+            new ArchLoader().LoadAssemblies(Assembly)
             .Build();
 
-        // might need a try catch
-        private static readonly string AssemblyName = typeof(Incident).Assembly.GetName().ToString().Split(",")[0];
+        private static readonly string AssemblyName = Assembly.GetName().ToString().Split(",")[0];       
 
-        // replace <ExampleClass> and <ForbiddenClass> with classes from the assemblies you want to test
-
-        //declare variables you'll use throughout your tests up here
-        //use As() to give them a custom description
-
-        /*
-        private readonly IObjectProvider<IType> ApplicationLayer =
-            Types().That().ResideInAssembly("ExampleAssembly").As("Application Layer"); // what assembly?
-
-        private readonly IObjectProvider<Class> ExampleClasses =
-            Classes().That().ImplementInterface("IExampleInterface").As("Example Classes");
-        */
-
+        // Application Layers
 
         private readonly IObjectProvider<IType> DomainLayer =
-           Types().That().ResideInNamespace(AssemblyName + ".Domain*", true).As("Domain Layer");
+           Types().That().ResideInNamespace(AssemblyName + DomainLayerNamespace, true).As(DomainLayerName);
 
         private readonly IObjectProvider<IType> ApplicationLayer =
-           Types().That().ResideInNamespace("MicroserviceTemplate.Application*", true).As("Application Layer");
-
-        private readonly IObjectProvider<Class> ServiceClasses =
-            Classes().That().HaveFullNameContaining("application").As("Service Classes");       
+           Types().That().ResideInNamespace(AssemblyName + ApplicationLayerNamespace, true).As(ApplicationLayerName);        
 
         private readonly IObjectProvider<IType> InfrastructureLayer =
-            Types().That()
-            .ResideInNamespace("MicroserviceTemplate.Infrastructure*", true)
-            //.ResideInNamespace("MicroserviceTemplate.Infrastructure.Interfaces")
-            .As("Infrastructure Layer");
+            Types().That().ResideInNamespace(AssemblyName + InfrastructureLayerNamespace, true).As(InfrastructureLayerName);
+
+        private readonly IObjectProvider<IType> ApiLayer =
+          Types().That().ResideInNamespace(AssemblyName + ApiLayerNamespace, true).As(ApiLayerName);
+
+        // Classes and Interfaces
+
+        private readonly IObjectProvider<Class> ServiceClasses =
+          Classes().That().HaveNameContaining(ServiceName).As(ServiceClassesName);
 
         private readonly IObjectProvider<Interface> RepositoryInterfaces =
-            Interfaces().That().HaveFullNameContaining("repository").As("Repository Interfaces");
+            Interfaces().That().HaveFullNameContaining(RepositoryName).As(RepositoryInterfacesName);
 
+        // Tests
 
-        //write some tests
         [Fact]
         public void TypesShouldBeInCorrectLayer()
         {
-            //you can use the fluent API to write your own rules
             // Arrange 
             IArchRule serviceClassesShouldBeInApplicationLayer =
                 Classes().That().Are(ServiceClasses).Should().Be(ApplicationLayer);
             IArchRule repositoryInterfacesShouldBeInInfrastructureLayer =
                 Interfaces().That().Are(RepositoryInterfaces).Should().Be(InfrastructureLayer);                 
 
-            // Act
-
-            //check if your architecture fulfils your rules
-            var res1 = serviceClassesShouldBeInApplicationLayer.HasNoViolations(Architecture);
-            var ev1 = serviceClassesShouldBeInApplicationLayer.Evaluate(Architecture);
-
-            var res2 = repositoryInterfacesShouldBeInInfrastructureLayer.HasNoViolations(Architecture);
-            var ev2 = repositoryInterfacesShouldBeInInfrastructureLayer.Evaluate(Architecture);
-
-            //you can also combine your rules
+            // Act           
             IArchRule combinedArchRule =
                 serviceClassesShouldBeInApplicationLayer.And(repositoryInterfacesShouldBeInInfrastructureLayer);
-            var result = combinedArchRule.HasNoViolations(Architecture);
-            var ev = combinedArchRule.Evaluate(Architecture);
+            var result = combinedArchRule.Evaluate(Architecture);
 
             // assert
-            res1.Should().Be(true);
-            ev1.Where(x => x.Passed == true).Should().NotBeEmpty();
-            res2.Should().Be(true);
-            ev2.Where(x => x.Passed == true).Should().NotBeEmpty();
-            result.Should().Be(true);
-            ev.Where(x => x.Passed == true).Should().NotBeEmpty();
+            result.Where(x => x.Passed == true).Should().NotBeEmpty();
         }
 
         [Fact]
-        public void DomainLayerShouldNotAccessApplicationLayer()
+        public void DomainLayerShouldNotAccessOtherLayers()
         {
-            //you can give your rules a custom reason, which is displayed when it fails
-            //(together with the types that failed the rule)
             // Arrange
             IArchRule domainLayerShouldNotAccessApplicationLayer = Types().That().Are(DomainLayer).Should()
-                .NotDependOnAny(ApplicationLayer).Because("it's forbidden");
+                .NotDependOnAny(ApplicationLayer).Because(DomainLayerRule);
+
+            IArchRule domainLayerShouldNotAccessInfracstrutureLayer = Types().That().Are(DomainLayer).Should()
+              .NotDependOnAny(InfrastructureLayer).Because(DomainLayerRule);
+
+            IArchRule domainLayerShouldNotAccessApiLayer = Types().That().Are(DomainLayer).Should()
+                .NotDependOnAny(ApplicationLayer).Because(DomainLayerRule);             
 
             // Act
-            var ev = domainLayerShouldNotAccessApplicationLayer.Evaluate(Architecture);
-            var res = domainLayerShouldNotAccessApplicationLayer.HasNoViolations(Architecture);
+            var result = domainLayerShouldNotAccessApplicationLayer
+                .And(domainLayerShouldNotAccessInfracstrutureLayer)
+                .And(domainLayerShouldNotAccessApiLayer)
+                .Evaluate(Architecture);
 
-            // Assert
-            res.Should().Be(true);
-            ev.Where(x => x.Passed == true).Should().NotBeEmpty();
+            // Asserts
+            result.Where(x => x.Passed == true).Should().NotBeEmpty();
         }
 
         [Fact]
@@ -114,32 +108,25 @@ namespace UnitTest
             var repositoryInterfacesShouldHaveRepositoryName = Classes().That().AreAssignableTo(RepositoryInterfaces).Should().HaveNameContaining("Repository");
 
             // Act
-            var ev = repositoryInterfacesShouldHaveRepositoryName.Evaluate(Architecture);
-            var res = repositoryInterfacesShouldHaveRepositoryName.HasNoViolations(Architecture);
+            var result = repositoryInterfacesShouldHaveRepositoryName.Evaluate(Architecture);
 
             // Assert
-            res.Should().Be(true, "because yes");
-            ev.Where(x => x.Passed == true).Should().NotBeEmpty();
+            result.Where(x => x.Passed == true).Should().NotBeEmpty();
         }
 
         [Fact]
         public void InfrastructureClassesShouldNotCallApplicationMethods()
         {
             // Arrange
-            var repositoryShouldNotCallApplicationOrServices = Classes().That().Are(RepositoryInterfaces).Should().NotCallAny(
-                    MethodMembers().That().AreDeclaredIn(ApplicationLayer).Or().HaveNameContaining("service"));
+            var repositoryShouldNotCallApplication = Classes().That().Are(RepositoryInterfaces).Should().NotCallAny(
+                    MethodMembers().That().AreDeclaredIn(ApplicationLayer));
 
             // Act
-            var ev = repositoryShouldNotCallApplicationOrServices
-                .Evaluate(Architecture);
-            var res = repositoryShouldNotCallApplicationOrServices
-               .HasNoViolations(Architecture);
+            var result = repositoryShouldNotCallApplication
+                .Evaluate(Architecture);            
 
-
-
-            // Assert
-            res.Should().Be(true);
-            ev.Where(x => x.Passed == true).Should().NotBeEmpty();
+            // Assert           
+            result.Where(x => x.Passed == true).Should().NotBeEmpty();
         }
     }
 }
